879644c421f617dbfdd0937b51df499b117bc80a
noah.emmenegger <noah.emmenegger@internezzo.ch>
2021-01-25 15:29:01 +0100

    init

---
+++ build.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const config = require('./config.json')
+
+if(fs.existsSync('build')) {
+    fs.rmdirSync('build', {recursive: true })
+}
+
+const buildFolder = fs.mkdirSync('build')
+
+const files = fs.readdirSync('.')
+
+
+function translate(key, language) {
+    return key + ' ' + language
+}
+
+
+function getTranslatedContent(file, language) {
+    let content =  fs.readFileSync(file)
+    let string = content.toString()
+
+    let matches = string.match(/translate\((.*?)\)/g)
+    matches.forEach(match => {
+        let key = match.match(/\((.*?)\)/g).toString().slice(1, -1)
+        string = string.replace(`translate(${key})`, translate(key, language))
+    })
+
+    return string
+}
+
+config.languages.forEach((language) => {
+    if(language !== config.defaultLanguage) {
+        fs.mkdirSync(`build/${language}`)
+
+        files.forEach((file) => {
+            if(file.endsWith('.html')) {
+                fs.writeFileSync(`build/${language}/${file}`, getTranslatedContent(file, language))
+            }
+        })
+    } else {
+        files.forEach((file) => {
+            if(file.endsWith('.html')) {
+                fs.writeFileSync(`build/${file}`, getTranslatedContent(file, 'en'))
+            }
+        })
+    }
+})
+
+return
